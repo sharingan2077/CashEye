b57f02815c25888996d15b03581ca9baa05728bd
Maksim Vishnevskiy <maksvish135@gmail.com>
2026-07-10 16:55:45 +0300

    feat: add date and action icons to navigation top bar

--- app/src/main/java/com/yandex/school/casheye/core/navigation/NavigationRoot.kt
+++ app/src/main/java/com/yandex/school/casheye/core/navigation/NavigationRoot.kt
@@ -1,17 +1,33 @@
 package com.yandex.school.casheye.core.navigation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Tune
+import androidx.compose.material.icons.outlined.CalendarMonth
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.ui.NavDisplay
+import com.yandex.school.casheye.R
 import com.yandex.school.casheye.feature.accounts.AccountScreen
 import com.yandex.school.casheye.feature.expense.ExpenseScreen
 import com.yandex.school.casheye.feature.income.IncomeScreen
@@ -79,8 +95,54 @@ fun NavigationRoot(
 @Composable
 private fun NavigationTopBar() {
     TopAppBar(
-        title = {},
-        actions = {},
+        title = {
+            Box(
+                modifier = Modifier
+                    .background(
+                        color = MaterialTheme.colorScheme.surfaceContainer,
+                        shape = CircleShape
+                    )
+                    .padding(horizontal = 12.dp, vertical = 6.dp)
+            ) {
+                Row(
+                    modifier = Modifier,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    Icon(
+                        Icons.Outlined.CalendarMonth,
+                        contentDescription = "Календарь"
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(
+                        text = "12 июня",
+                        style = MaterialTheme.typography.labelLarge,
+                        color = MaterialTheme.colorScheme.onSurface,
+                    )
+                }
+            }
+        },
+        actions = {
+            Box(
+                modifier = Modifier
+                    .size(48.dp),
+                contentAlignment = Alignment.Center
+            ) {
+            Icon(
+                painter = painterResource(R.drawable.analytics),
+                contentDescription = "Аналитика"
+            )
+            }
+            Box(
+                modifier = Modifier.size(48.dp),
+                contentAlignment = Alignment.Center
+            ) {
+            Icon(
+                imageVector = Icons.Default.Tune,
+                contentDescription = "Фильтры"
+            )
+            }
+        },
     )
 }
 
@@ -89,15 +151,5 @@ private fun NavigationTopBar() {
 private fun NavigationTopBarPreview() {
     CashEyeTheme(dynamicColor = false) {
         NavigationTopBar()
-    }
-}
-
-@Preview(showBackground = true, widthDp = 412, heightDp = 892)
-@Composable
-private fun NavigationRootPreview() {
-    CashEyeTheme(dynamicColor = false) {
-        Surface {
-            NavigationRoot()
-        }
     }
 }
