2aaa6d621465cd3c6f9aea61c8747e9c0962cef8
Maksim Vishnevskiy <maksvish135@gmail.com>
2026-07-11 11:25:02 +0300

    feat: match navigation top bar to design

--- app/src/main/java/com/yandex/school/casheye/core/navigation/NavigationRoot.kt
+++ app/src/main/java/com/yandex/school/casheye/core/navigation/NavigationRoot.kt
@@ -1,39 +1,26 @@
 package com.yandex.school.casheye.core.navigation
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Tune
-import androidx.compose.material.icons.outlined.CalendarMonth
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.ui.NavDisplay
-import com.yandex.school.casheye.R
 import com.yandex.school.casheye.feature.accounts.AccountScreen
 import com.yandex.school.casheye.feature.expense.ExpenseScreen
 import com.yandex.school.casheye.feature.income.IncomeScreen
 import com.yandex.school.casheye.ui.theme.CashEyeTheme
+import java.time.LocalDate
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -61,10 +48,14 @@ fun NavigationRoot(
                 }
             )
         },
-        topBar = { NavigationTopBar() },
-        floatingActionButton = { FloatingButton { 
-            // TODO: Implement onCLick
-        } }
+        topBar = {
+            NavigationTopBar(LocalDate.of(2026, 6, 12))
+        },
+        floatingActionButton = {
+            FloatingButton {
+                // TODO: Implement onCLick
+            }
+        }
     ) { innerPadding ->
         NavDisplay(
             modifier = Modifier
@@ -112,69 +103,6 @@ private fun FloatingButton(onClick: () -> Unit) {
                 .size(24.dp)
         )
 
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun NavigationTopBar() {
-    TopAppBar(
-        title = {
-            Box(
-                modifier = Modifier
-                    .background(
-                        color = MaterialTheme.colorScheme.surfaceContainer,
-                        shape = CircleShape
-                    )
-                    .padding(horizontal = 12.dp, vertical = 6.dp)
-            ) {
-                Row(
-                    modifier = Modifier,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-
-                    Icon(
-                        Icons.Outlined.CalendarMonth,
-                        contentDescription = "Календарь"
-                    )
-                    Spacer(modifier = Modifier.width(4.dp))
-                    Text(
-                        text = "12 июня",
-                        style = MaterialTheme.typography.labelLarge,
-                        color = MaterialTheme.colorScheme.onSurface,
-                    )
-                }
-            }
-        },
-        actions = {
-            Box(
-                modifier = Modifier
-                    .size(48.dp),
-                contentAlignment = Alignment.Center
-            ) {
-            Icon(
-                painter = painterResource(R.drawable.analytics),
-                contentDescription = "Аналитика"
-            )
-            }
-            Box(
-                modifier = Modifier.size(48.dp),
-                contentAlignment = Alignment.Center
-            ) {
-            Icon(
-                imageVector = Icons.Default.Tune,
-                contentDescription = "Фильтры"
-            )
-            }
-        },
-    )
-}
-
-@Preview(showBackground = true, widthDp = 412)
-@Composable
-private fun NavigationTopBarPreview() {
-    CashEyeTheme(dynamicColor = false) {
-        NavigationTopBar()
     }
 }
 
