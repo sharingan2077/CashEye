4feb932878df22c786911b596837dff0de52dac0
Maksim Vishnevskiy <maksvish135@gmail.com>
2026-07-26 23:36:37 +0300

    refactor: simplify success snackbar

--- app/src/main/java/com/yandex/school/casheye/app/navigation/NavigationRoot.kt
+++ app/src/main/java/com/yandex/school/casheye/app/navigation/NavigationRoot.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -268,9 +269,7 @@ private fun NavigationScaffold(
             }
         },
         snackbarHost = {
-            CashEyeSnackbarHost(
-                hostState = snackbarHostState,
-            )
+            SnackbarHost(hostState = snackbarHostState)
         },
     ) { innerPadding ->
         NavigationContent(
